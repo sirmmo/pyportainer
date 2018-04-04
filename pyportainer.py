af0561bcbe11d9c82a6b8e5ddc175386aba7aa04
Marco Montanari <sirmmo@gmail.com>
2018-04-04 09:11:21 +0000

    update

--- pyportainer.py
+++ pyportainer.py
@@ -7,43 +7,120 @@ class PyPortainer():
         self.portainer_endpoint = portainer_endpoint+"/api"
     
     def login(self, username, password):
-        r = requests.post(self.portainer_endpoint+"/auth", data=json.dumps({"Username":username, "Password":password}), verify=False)
+        r = requests.post(
+            self.portainer_endpoint+"/auth", 
+            data=json.dumps({"Username":username, "Password":password}), 
+            verify=False)
         j = r.json()
         self.token = j.get("jwt")
     
-    def get_dockerhub(self):
-        pass
-    def put_dockerhub(self, options):
-        pass
+    
+    
+    def get_dockerhub_info(self):
+        r = requests.get(
+            self.portainer_endpoint+"/dockerhub", 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
+    def put_dockerhub_info(self, options):
+        r = requests.put(
+            self.portainer_endpoint+"/dockerhub", 
+            data=json.dumps(options), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
+        
     
     def get_endpoints(self):
-        r = requests.get(self.portainer_endpoint+"/endpoints", headers={"Authorization": "Bearer {}".format(self.token)}, verify=False)
+        r = requests.get(
+            self.portainer_endpoint+"/endpoints", 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
         return r.json()
         
     def new_endpoints(self, options):
-        pass
+        r = requests.post(
+            self.portainer_endpoint+"/endpoints", 
+            data=json.dumps(options), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     def get_endpoint(self, identifier):
-        pass
+        r = requests.get(
+            self.portainer_endpoint+"/endpoints/{}".format(identifier), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     def update_endpoint(self, identifier, options):
-        pass
+        r = requests.put(
+            self.portainer_endpoint+"/endpoints/{}".format(identifier), 
+            data=json.dumps(options), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     def delete_endpoint(self, identifier):
-        pass
+        r = requests.delete(
+            self.portainer_endpoint+"/endpoints/{}".format(identifier), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     def access_endpoint(self, identifier, options):
-        pass
+        r = requests.put(
+            self.portainer_endpoint+"/endpoints/{}/access".format(identifier), 
+            data=json.dumps(options), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     
     def get_stacks(self, endpoint):
-        r = requests.get(self.portainer_endpoint + "/endpoints/{}/stacks".format(endpoint), headers={"Authorization": "Bearer {}".format(self.token)}, verify=False)
+        r = requests.get(
+            self.portainer_endpoint + "/endpoints/{}/stacks".format(endpoint), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
         return r.json()
         
     def new_stack(self, endpoint, options):
-        pass
-    def inspect_stack(self, endpoint, stack):
-        pass
-    def update_stack(self, endpoint, stack, options):
-        pass
-    def delete_stack(self, endpoint, stack):
-        pass
+        r = requests.post(
+            self.portainer_endpoint + "/endpoints/{}/stacks".format(endpoint), 
+            data=json.dumps(options),
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     def get_stack(self, endpoint, stack):
-        pass
+        r = requests.get(
+            self.portainer_endpoint + "/endpoints/{}/stacks/{}".format(endpoint, stack), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
+    def update_stack(self, endpoint, stack, options):
+        r = requests.put(
+            self.portainer_endpoint + "/endpoints/{}/stacks/{}".format(endpoint, stack), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
+    def delete_stack(self, endpoint, stack):
+        r = requests.delete(
+            self.portainer_endpoint + "/endpoints/{}/stacks/{}".format(endpoint, stack), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
+    def get_stackfile(self, endpoint, stack):
+        r = requests.get(
+            self.portainer_endpoint + "/endpoints/{}/stacks/{}/stackfile".format(endpoint, stack), 
+            headers={"Authorization": "Bearer {}".format(self.token)}, 
+            verify=False)
+        return r.json()
+        
     
     
